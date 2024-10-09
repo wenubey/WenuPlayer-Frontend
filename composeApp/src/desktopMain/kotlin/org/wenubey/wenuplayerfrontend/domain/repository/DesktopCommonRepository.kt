8499c24ef2bf1969287076aa458dbf1c044053af
mert <mertfatihsimsek06@gmail.com>
2024-10-09 14:53:26 +0200

    Internet connection check fixed on desktop side.

--- composeApp/src/desktopMain/kotlin/org/wenubey/wenuplayerfrontend/domain/repository/DesktopCommonRepository.kt
+++ composeApp/src/desktopMain/kotlin/org/wenubey/wenuplayerfrontend/domain/repository/DesktopCommonRepository.kt
@@ -1,20 +1,30 @@
 package org.wenubey.wenuplayerfrontend.domain.repository
 
+import co.touchlab.kermit.Logger
 import org.wenubey.wenuplayerfrontend.data.dto.VideoSummary
 import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.net.InetAddress
+import java.net.NetworkInterface
 import javax.imageio.ImageIO
 import javax.swing.ImageIcon
 import javax.swing.filechooser.FileSystemView
 
 class DesktopCommonRepository : CommonRepository {
-
+    private val logger = Logger.withTag("DesktopCommonRepository")
     override fun hasInternetConnection(): Boolean {
         return try {
-            InetAddress.getByName("google.com").isReachable(5000)
+            val networkInterfaces = NetworkInterface.getNetworkInterfaces()
+            while (networkInterfaces.hasMoreElements()) {
+                val networkInterface = networkInterfaces.nextElement()
+                if (networkInterface.isUp && !networkInterface.isLoopback) {
+                    return true
+                }
+            }
+            false
         } catch (e: Exception) {
+            logger.e { "${e.message}" }
             false
         }
     }
